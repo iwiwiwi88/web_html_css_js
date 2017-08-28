dce0ea329cdbe9d7b7d128ba63b186c13c308cd3
Iwona Sasak <sasak.iwona.88@gmail.com>
2017-08-28 16:02:11 +0200

    JQuery - codePlayer toggling works

--- JQuery/codePlayer.js
+++ JQuery/codePlayer.js
@@ -3,3 +3,15 @@ var menuBarHeight=$("#menuBar").height();
 var codeContainerHeight=windowHeight;
 
 $(".codeContainer").height(codeContainerHeight+"px");
+
+$(".toggle").click(function() {
+	$(this).toggleClass("selected");
+	var activeDiv=$(this).html();
+	//alert(activeDiv);
+	$("#"+activeDiv+"Container").toggle();
+	var showingDivs=$(".codeContainer").filter(function() {
+		return($(this).css("display")!="none");
+	}).length;
+	var width=100/showingDivs;
+	$(".codeContainer").width(width+"%");
+});
