eefa8fb20ebb5fb5f961d58dd9c9b15a6e6e6557
Iwona Sasak <sasak.iwona.88@gmail.com>
2017-08-29 15:46:45 +0200

    PHP folder created - I'm scared...

---
+++ PHP/index.php
@@ -0,0 +1,19 @@
+<html>
+   <head>
+      <title>Online PHP Script Execution</title>      
+   </head>
+   <body> 
+		<?php
+			echo "<h1>Hello, PHP!</h1>";
+			$test="Variable1";
+			$test2="Variable2";
+			$num=10;
+			echo $test.$test2;
+			echo $num/5;
+			$name="Iwiwiwi";
+			echo "My name is $name";
+			$a="name";
+			echo $$a;
+		?>
+	</body>
+</html>
